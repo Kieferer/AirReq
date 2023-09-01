503ba07fb969007807ce1603e4f361c96de7dbf4
Kieferer <balazsborsi0@gmail.com>
2023-09-01 20:42:22 +0200

    feat: Import MatDividerModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import {MatDividerModule} from "@angular/material/divider";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatDividerModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
