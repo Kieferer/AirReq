2285d2b3c61006a598977fea80b204b849f47e27
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-09-08 15:14:28 +0200

    feat: Create collections-content component to hold visualization of collections

---
+++ src/app/collections-content/collections-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collections-content',
+  templateUrl: './collections-content.component.html',
+  styleUrls: ['./collections-content.component.css']
+})
+export class CollectionsContentComponent {
+
+}
