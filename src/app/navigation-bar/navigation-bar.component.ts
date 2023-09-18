cceadaa900ea2c507d17f033c0f3f344b77a88b5
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-09-18 14:36:27 +0200

    feat: Create 'closeApp' method to be able to close the application by clicking the close button, the button calls the method

--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {ApplicationModule, Component} from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./navigation-bar.component.css']
 })
 export class NavigationBarComponent {
-
+  closeApp() {}
 }
